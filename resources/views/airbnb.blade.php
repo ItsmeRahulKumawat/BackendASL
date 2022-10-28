ec64a60bab1cb40857342d1061d26adc4ea21af2
[Rahul Kumawat] <[rahulkumawat20000@gmail.com]>
2022-10-28 16:17:14 +0530

    Backend

---
+++ resources/views/airbnb.blade.php
@@ -0,0 +1,61 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" type="text/css" href="./airbnb.css" />
+        <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+        />
+        <!-- CSS only -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+            crossorigin="anonymous"
+        />
+        <!-- JavaScript Bundle with Popper -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+            crossorigin="anonymous"
+        ></script>
+        <title>Airbnb</title>
+    </head>
+    <body>
+        <div class="container text-lg-left">
+            <div class="row">
+                <div class="col col-lg-1 logodiv">
+                    <img
+                        class="logo img-fluid"
+                        src="./images/logo.jpg"
+                        alt=""
+                    />
+                </div>
+                <div class="col col-lg-1">
+                    <img class="logo2" src="./images/text.jpg" alt="" />
+                </div>
+                <div class="col col-lg-1"></div>
+                <div class="col col-lg-1"></div>
+                <div class="col col-lg-1"></div>
+                <div class="col col-lg-1"></div>
+                <div class="col col-lg-1"></div>
+                <div class="col col-lg-1"></div>
+                <div class="col col-lg-1"></div>
+                <div class="col col-lg-1">
+                    <input
+                        class="topbutton"
+                        type="button"
+                        value="Become a host"
+                    />
+                </div>
+                <div class="col col-lg-1">
+                    <span class="globe material-symbols-outlined">
+                        language
+                    </span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
